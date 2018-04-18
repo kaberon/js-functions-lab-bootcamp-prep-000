51fc366f92fa001246104d9ec114f2c74f33f3c3
Ben Kaberon <kaberon@gmail.com>
2018-04-18 19:09:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ function happyHolidays() {
   return "Happy holidays!";
 }
 
-function happyHolidaysTo(name) {
-return "Happy holidays, ${name}!";
-  
+ 
+ function happyHolidaysTo(name) {
+  return(`Happy holidays, ${name}!`)
 }
- happyHolidaysTo(you);
+ 
+happyHolidaysTo('you')
